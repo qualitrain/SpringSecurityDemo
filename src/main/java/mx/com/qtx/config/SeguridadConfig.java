446d62a4f06719b0e41e3d245b63f1aa48e14c87
Alex Cruz <acruz@qualitrain.com.mx>
2020-08-16 16:38:18 -0500

    Configuración para cargar usuarios a partir de arreglos. Debug ON

--- src/main/java/mx/com/qtx/config/SeguridadConfig.java
+++ src/main/java/mx/com/qtx/config/SeguridadConfig.java
@@ -2,6 +2,7 @@ package mx.com.qtx.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -11,13 +12,28 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SeguridadConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	//	super.configure(auth);
-		auth.inMemoryAuthentication()
-			.withUser("alex").password("tekamachalko").roles("USER","AGENTE","ADMIN")
-		    .and()
-		    .withUser("david").password("tekolutla").roles("AGENTE")
-		    .and()
-		    .withUser("tavo").password("tlatelolko").roles("LOGISTICA");
+		String usuarios[] = {
+							"alex",
+							"david",
+							"tavo",
+							"benito"
+							};
+		String passwords[] = {
+							"tekamachalko",
+							"tekolutla",
+							"tlatelolko",
+							"bodoque"
+							};
+		String roles[][] = { 
+				   		{ "USER", "AGENTE", "ADMIN"},
+				   		{ "AGENTE"},
+				   		{ "LOGISTICA"},
+				   		{ "CLIENTE"}
+		 		 };
+		InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> autMem = auth.inMemoryAuthentication();
+		for(int i=0; i<usuarios.length; i++) {
+			autMem.withUser(usuarios[i]).password(passwords[i]).roles(roles[i]);
+		}
 	}
 	@Bean
 	public PasswordEncoder getPasswordEncoder(){
